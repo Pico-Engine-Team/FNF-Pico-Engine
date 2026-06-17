8e5218b84c43a9dbe172bc269ab21d7f550b7bd7
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-06-17 13:25:54 -0300

    Update PlayState.hx

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -26,7 +26,7 @@ import funkin.data.objects.game.notes.data.StrumNote;
 import funkin.data.objects.game.notes.data.NoteSplash;
 import funkin.data.objects.game.notes.data.HoldNoteCover;
 import funkin.data.objects.game.notes.data.Note.EventNote;
-import import funkin.data.objects.game.notes.config.NoteTypesConfig;
+import funkin.data.objects.game.notes.config.NoteTypesConfig;
 import funkin.states.PauseState;
 import funkin.states.GameOverState;
 
