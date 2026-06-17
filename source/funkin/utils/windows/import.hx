3000cc0583347eca12affcad49966b43dc52a7ab
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-06-17 15:30:26 -0300

    Add import.hx

---
+++ source/funkin/utils/windows/import.hx
@@ -0,0 +1,7 @@
+package funkin.utils.windows;
+
+import funkin.utils.windows.DebugDisplay;
+import funkin.utils.windows.Main;
+import funkin.utils.windows.WindowsCPP;
+import funkin.utils.windows.cpp.*;
+import funkin.utils.windows.winGDIThings.*;
