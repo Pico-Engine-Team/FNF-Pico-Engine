63f2a1a8de6fd35e3bb8868b7645faadf340c96f
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-06-30 03:14:50 -0300

    Update Import (v0.0.1)

--- source/funkin/utils/import.hx
+++ source/funkin/utils/import.hx
@@ -1,16 +1,12 @@
 package funkin.utils;
 
 import funkin.utils.Alphabet;
-import funkin.utils.ALSoftConfig;
-import funkin.utils.Conductor;
 import funkin.utils.Controls;
 import funkin.utils.CoolUtil;
 import funkin.utils.CustomFadeTransition;
 import funkin.utils.EditorsMenus;
 import funkin.utils.InputFormatter;
-import funkin.utils.Language;
 import funkin.utils.MathUtils;
 import funkin.utils.Native;
 import funkin.utils.TypedAlphabet;
 import funkin.utils.api.DiscordAPI;
-import funkin.utils.api.WindowsAPI;
