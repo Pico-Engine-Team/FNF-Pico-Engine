666a1a20d9938c91e18e5f5c2092c037acc4ddbd
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-06-30 03:11:01 -0300

    Update import.hx

--- source/funkin/utils/windows/import.hx
+++ source/funkin/utils/windows/import.hx
@@ -1,7 +1,6 @@
 package funkin.utils.windows;
 
 import funkin.utils.windows.DebugDisplay;
-import funkin.utils.windows.Main;
 import funkin.utils.windows.WindowsCPP;
-import funkin.utils.windows.cpp.*;
-import funkin.utils.windows.winGDIThings.*;
+import funkin.utils.windows.data.cpp.*;
+import funkin.utils.windows.data.winGDIThings.*;
