7519709410e2356be0e65ec433bb880334229bb2
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-07-01 01:41:48 -0300

    Delete source/funkin/utils directory

--- source/funkin/utils/windows/import.hx
+++
@@ -1,6 +0,0 @@
-package funkin.utils.windows;
-
-import funkin.utils.windows.DebugDisplay;
-import funkin.utils.windows.WindowsCPP;
-import funkin.utils.windows.data.cpp.*;
-import funkin.utils.windows.data.winGDIThings.*;
