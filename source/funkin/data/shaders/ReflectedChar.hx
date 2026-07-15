0d7b5c8afd32fd08cdeb97abb131ad1d1b68258c
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-07-15 18:35:01 -0300

    New Shader By Torch Engine

---
+++ source/funkin/data/shaders/ReflectedChar.hx
@@ -0,0 +1,55 @@
+package funkin.data.shaders;
+
+import funkin.data.objects.game.characters.Character;
+import flixel.system.FlxAssets.FlxShader;
+
+class ReflectedChar extends Character
+{
+    var charRef:Character = null;
+    public function new(char:Character, ?alpha:Float = 0.35)
+    {
+        super(char.x, char.y + (char.frameHeight*char.scale.y*2) - char.offset.y + checkCharacter(char.curCharacter), char.curCharacter, char.isPlayer);
+        charRef = char;
+        this.alpha = alpha;
+        this.flipY = true;
+        this.shader = charRef.shader;
+        this.dance();
+    }
+
+    // only here if it didn't work initially
+    public function setCharRef(char:Character) {
+        charRef = char;
+        this.x = charRef.x;
+        this.y = charRef.y + (charRef.frameHeight*charRef.scale.y*2) - charRef.offset.y + checkCharacter(charRef.curCharacter);
+    }
+
+    // Will eventually try to make this into the character file
+    function checkCharacter(name:String):Float
+    {
+        switch (name) {
+            case 'pico' | 'pico-playable' | 'pico-blazin':
+                return -65;
+            case 'gf':
+                return -10;
+            case 'bf-pixel-opponent':
+                return -325;
+                return -75;
+            default: // 'bf' is used at the default here.
+                return -55;
+        }
+    }
+
+    override function update(elapsed:Float)
+    {
+        this.shader = charRef.shader;
+        this.offset.x = charRef.offset.x;
+        this.offset.y = (charRef.frameHeight*charRef.scale.y) - charRef.offset.y;
+        if (charRef.isAnimateAtlas == true && charRef.atlas.anim.curSymbol != null) {
+            this.atlas.anim.play(charRef.atlas.anim.curSymbol.name, true, false, charRef.atlas.anim.curSymbol.curFrame);
+        } else if (charRef.animation.curAnim != null) {
+            this.animation.play(charRef.animation.curAnim.name, true, false, charRef.animation.curAnim.curFrame);
+        }
+
+        super.update(elapsed);
+    }
+}
