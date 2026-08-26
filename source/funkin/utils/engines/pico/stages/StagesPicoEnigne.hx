38b043517d989ee6b2b907844080d65740fb9669
Lucas-Sanches <lucas.smbm.2008@gmail.com>
2026-08-26 15:30:32 -0300

    Update File

--- source/funkin/utils/engines/pico/stages/StagesPicoEnigne.hx
+++ source/funkin/utils/engines/pico/stages/StagesPicoEnigne.hx
@@ -1,21 +1,22 @@
 #if PICO_ALLOWED
 package funkin.utils.engines.pico.stages;
 
-import funkin.stages.data.levels.weekspecial.engine.standard.*;
-import funkin.stages.data.levels.weekspecial.engine.mods.*;
-import funkin.stages.data.levels.weekspecial.engine.retake.*;
-import funkin.stages.data.levels.weekspecial.engine.mods.exe.*;
-import funkin.stages.data.levels.weekspecial.engine.mods.exe.encore.*;
-import funkin.stages.data.levels.weekspecial.engine.mods.funkadelix.*;
+import funkin.stages.data.weekspecial.engine.standard.*;
+import funkin.stages.data.weekspecial.engine.mods.*;
+import funkin.stages.data.weekspecial.engine.retake.*;
+import funkin.stages.data.weekspecial.engine.mods.exe.*;
+import funkin.stages.data.weekspecial.engine.mods.exe.encore.*;
+import funkin.stages.data.weekspecial.engine.mods.funkadelix.*;
 
 import funkin.stages.BaseStage;
-import funkin.modding.scripting.FunkinLua;
+import funkin.modding.scripting.FunkinLuaProgramming;
 import haxe.ds.List;
 
-class StagesPicoEnigne extends BaseStage {
+class StagesPicoEnigne extends BaseStage
+{
     public static var currentStage:BaseStage = null;
     #if LUA_ALLOWED
-    public static function implement(funk:FunkinLua) {
+    public static function implement(funk:FunkinLuaProgramming) {
         var lua:State = funk.lua;
         funk.set('versionPS', MainMenuState.PicoVersion.trim());
     }
@@ -26,13 +27,13 @@ class StagesPicoEnigne extends BaseStage {
             currentStage = null;
             currentStage = switch (name)
             {
-                case "endlessEncore": new EndlessEncore();         //Sonic.exe Vs Sonic Majin
-                case 'castleBowser': new CastleBowser();           //Vs Bowser (Pico Mix)
-                case 'matt-arena': new Arenanew();                 //Vs Matt (Pico Mix)
-                case 'stageSky': new StageSky();                   //Reteke Sky (Pico Mix)
-                case 'tankReteke': new TankReteke();			   //Week 7 Retake
-                //case 'HappyRon': new Ron();						   //Vs Happy Ron (Pico Mix)
-                case 'whittyAlley': new WhittyAlley();             //Vs Whitty (Pico Mix)
+                case "endlessEncore": new EndlessEncore();         // Sonic.exe Vs Sonic Majin
+                case 'castleBowser': new CastleBowser();           // Vs Bowser (Pico Mix)
+                case 'matt-arena': new Arenanew();                 // Vs Matt (Pico Mix)
+                case 'stageSky': new StageSky();                   // Reteke Sky (Pico Mix)
+                case 'tankReteke': new TankReteke();			   // Week 7 Retake
+                //case 'HappyRon': new Ron();					   // Vs Happy Ron (Pico Mix)
+                case 'whittyAlley': new WhittyAlley();             // Vs Whitty (Pico Mix)
             default: null;
         }
         #end
